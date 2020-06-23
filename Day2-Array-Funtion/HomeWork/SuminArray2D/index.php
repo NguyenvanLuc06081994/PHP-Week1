a6d844ed7c63dff9a948c45b267a1fd15722206b
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-06-23 15:26:55 +0700

    add sum file

---
+++ Day2-Array-Funtion/HomeWork/SuminArray2D/index.php
@@ -0,0 +1,17 @@
+<?php
+function sum($arr)
+{
+    $sum = 0;
+    for ($i = 0; $i < count($arr); $i++) {
+        for ($j = 0; $j < count($arr[$i]); $j++) {
+            if ($i === $j) {
+                $sum += $arr[$i][$j];
+            }
+        }
+    }
+    return $sum;
+}
+
+echo sum([[1, 2, 3],
+    [3, 4, 5],
+    [6, 7, 8]]);
